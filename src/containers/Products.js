e5270b68bc47976db49bcb38e026e68557f4ae89
Onder Gunacan <ondergunacan@Onders-MacBook-Air.local>
2019-07-29 15:25:25 -0600

    bootstrap cards added to the products page

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -12,12 +12,37 @@ class Products extends Component {
             .then(res => res.json())
             .then(products => {
                 console.log(products)
+                setTimeout(() => {
+                    this.setState({
+                        products : products,
+                        isLoading : false
+                    })
+                }, 1000);
             });
     }
 
     render () {
         return (
-            <h1>Products Component</h1>
+            <div>
+                <h1>Products Page</h1>
+                {this.state.isLoading ? 
+                    <h2>Loading Stickers...</h2> : 
+                    <div className='row'>
+                        {
+                            this.state.products.map(product => (
+                                <div className="card col-4" >
+                                    <img src={product.url} className="card-img-top" alt={product.title} />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{product.title}</h5>
+                                        <p className="card-text">{product.description}</p>
+                                        <a href="#" className="btn btn-primary">View product</a>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                }
+            </div>
         )
     }
 }
