0be5f829f5225411b3ee068b98a7dd0f6665bdcd
Onder Gunacan <ondergunacan@Onders-MacBook-Air.local>
2019-08-19 13:46:20 -0600

    complete controlled form to create a sticker

--- src/containers/ProductForm.js
+++ src/containers/ProductForm.js
@@ -5,17 +5,42 @@ class ProductForm extends Component {
         product: {}
     };
 
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState((prevState) => ({
+            product: {
+                ...prevState.product,
+                [name]: value
+                
+            }
+        }))
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state.product)
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="title">Title</label>
-                    <input type="text" className="form-control" id="title"  placeholder="Enter a Title" required />
+                    <input onChange={this.handleChange} name='title' type="text" className="form-control" id="title"  placeholder="Enter a Title" required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="description">Description</label>
-                    <textarea className="form-control" id="description" placeholder='Enter a Description' rows="3" required></textarea>
+                    <textarea onChange={this.handleChange} name='description' className="form-control" id="description" placeholder='Enter a Description' rows="3" required></textarea>
                 </div>
+                <div className="form-group">
+                    <label htmlFor="rating">Rating</label>
+                    <input onChange={this.handleChange} name='rating' type="number" step='0.5' min='1' max='10' className="form-control" id="rating"  placeholder="Enter a Number 1 to 10" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="image">Image</label>
+                    <input onChange={this.handleChange} name='url' type="url" className="form-control" id="image"  placeholder="http://example.com/image.jpg" required />
+                </div>
+                
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
